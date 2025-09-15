7048cc5a827f08902fa8ed46fcbcdb4865653375
Fredeblok0912 <frederikblokhellesoe@gmail.com>
2025-09-15 12:46:02 +0200

    added classes for 3 enemies and a player

--- character.gd
+++ character.gd
@@ -1,8 +1,19 @@
 extends Node2D
-
 class_name Character
 
-var health = 100
-var shield = 0
-
-@onready var player_sprite: Sprite2D = $Sprite2d
+@export var health: int = 100
+@export var max_health: int = 100
+@export var shield: int = 0
+@export var sprite: Sprite2D 
+@export var health_bar: ProgressBar
+		
+	
+func hide_sprite():
+	sprite.hide()
+	
+func die():
+	queue_free()
+	
+func _ready():
+	if not sprite and has_node("Sprite2D"):
+		sprite = $Sprite2D
---
+++ player.gd
@@ -0,0 +1,6 @@
+extends Character
+class_name Player
+
+
+func _ready():
+	hide_sprite()
