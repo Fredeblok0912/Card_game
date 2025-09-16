c0a93384c21c15355d52720c6e124eb4b9b6b07d
CoinLords <eliot.rc@outlook.fr>
2025-09-16 16:58:15 +0200

    shield now works

--- Player_shield_text.gd
+++ Player_shield_text.gd
@@ -1,11 +1,8 @@
 extends RichTextLabel
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if player.player_shield > 0:
+		set_text(str(player.player_shield))
+	else:
+		set_text("")
--- player.gd
+++ player.gd
@@ -5,7 +5,11 @@ var player_max_health = 25
 var player_shield = 0
 
 func take_damage(amount):
-	player_health = player_health - amount
+	for i in amount:
+		if player_shield != 0:
+			player_shield = player_shield -1
+		else:
+			player_health = player_health - 1
 
 func gain_shield(amount):
 	player_shield = player_shield + amount
