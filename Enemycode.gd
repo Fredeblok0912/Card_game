225a19e882bcf84376b78778956f4e61464941af
CoinLords <eliot.rc@outlook.fr>
2025-09-16 16:29:27 +0200

    Update Enemycode.gd

--- Enemycode.gd
+++ Enemycode.gd
@@ -9,3 +9,16 @@ func _attack():
 	
 func _choose():
 	pass
+
+
+#moveset slime
+# - attack deal 5 damage x difficulty_mod
+# - attack deal 8 damage x difficulty_mod
+# - shield gain 4 block x difficulty_mod
+# - heal/shield regain 3 health and get 2 sheild x difficulty_mod
+
+#moveset zombie
+# - attack/heal deal 4 damage and regain 2 health x difficulty_mod
+# - attack/shield deal 6 damage and gian 3 shield x difficulty_mod
+# - shield gain 4 block x difficulty_mod
+# - heal regain 5 health x difficulty_mod
