fef068069d108ce54aa2096fcaab53570272a456
CoinLords <eliot.rc@outlook.fr>
2025-09-16 17:01:54 +0200

    added gameover when player dies

--- game.gd
+++ game.gd
@@ -1,4 +1,1 @@
 extends Node2D
-
-func game_over():
-	get_tree().change_scene_to_file("res://game_over.tscn")
--- player.gd
+++ player.gd
@@ -10,6 +10,13 @@ func take_damage(amount):
 			player_shield = player_shield -1
 		else:
 			player_health = player_health - 1
+	if not player_health > 0:
+		game_over()
+		
 
 func gain_shield(amount):
 	player_shield = player_shield + amount
+
+func game_over():
+	get_tree().change_scene_to_file("res://game_over.tscn")
+	
