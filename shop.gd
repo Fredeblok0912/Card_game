46c062067593018c483c0693d0d947c9f02f59a7
CoinLords <eliot.rc@outlook.fr>
2025-09-17 15:30:51 +0200

    added $$$signs

--- shop.gd
+++ shop.gd
@@ -11,7 +11,7 @@ var rarity_price = {
 
 func _ready():
 	display_cards()
-	label.text = "Money: " + str(player.money)
+	label.text = "Money: " + str(player.money)+"$"
 	
 
 func buy_cards(card_id, price):
@@ -83,7 +83,8 @@ func display_cards():
 		var sprite_size = card_sprite.texture.get_size() * card_sprite.scale  # Vector2
 
 		var price_label := Label.new()
-		price_label.text = str(Cardlist.card_database[card_id].get("cost"))
+		price_label.text = ("")
+		price_label.text = str(rarity_price[Cardlist.card_database[card_id]["rarity"]].get("price"),"$")
 		price_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 		price_label.size_flags_horizontal = Control.SIZE_FILL
 		price_label.size = Vector2(sprite_size.x, 20)# grrr jeg fucking hader 
