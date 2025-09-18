bf9cf09877e60b1b336604babb811f106a7d31d1
CoinLords <eliot.rc@outlook.fr>
2025-09-18 22:21:32 +0200

    SFX and MUSIC

--- spirte_bullshit.gd
+++ spirte_bullshit.gd
@@ -1,20 +1,25 @@
 extends Node2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var ShieldUpSFX = $ShieldUp
+@onready var SwordSwingSFX = $SwordSwing
+@onready var BackgroundMusic = $Music
 
 func _ready() -> void:
 	IdleAnimation()
+	BackgroundMusic.play()
 
 func SwingAnimation():
 	animated_sprite.play("Swordswing")
+	SwordSwingSFX.play()
 	await animated_sprite.animation_finished
 	animated_sprite.play("Idle")
 
 func BlockAnimation():
 	animated_sprite.play("Swordblock")
+	ShieldUpSFX.play()
 	await SpriteControl.animated_sprite.animation_finished
 	animated_sprite.play("Idle")
-	
 	
 func IdleAnimation():
 	animated_sprite.play("Idle")
