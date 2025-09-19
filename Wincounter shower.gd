95e5fc784d3188cf5aab87e446c75efeeb71c9de
Fredeblok0912 <frederikblokhellesoe@gmail.com>
2025-09-19 15:33:13 +0200

    Revert "finished gameover screen"
    This reverts commit 215e446226b8bb957e7d2015590ca611f830d7cb.

--- Wincounter shower.gd
+++ Wincounter shower.gd
@@ -1,8 +1,4 @@
-extends RichTextLabel
-
-func _ready() -> void:
-	bbcode_enabled = true
+extends Label
 
 func _process(delta: float) -> void:
-	$".".scale = Vector2(2, 2)
-	$".".text = "[center]%s[/center]" % str(Game.Wincounter)
+	$".".text = "you survived "+ str(Game.Wincounter) +" Rounds"
